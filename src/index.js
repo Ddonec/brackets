dc610f073f7f1b2132e2faf32ad74d9f45928ee6
Anton <137609245+Ddonec@users.noreply.github.com>
2023-09-24 18:02:51 +0300

    feat: I couldn't solve 100% later I'll try again

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const stack = [];
+  const openingBrackets = [];
+  const closingBrackets = [];
+  const bracketMap = new Map();
+
+  for (const [open, close] of bracketsConfig) {
+    openingBrackets.push(open);
+    closingBrackets.push(close);
+    bracketMap.set(close, open);
+  }
+
+  for (const char of str) {
+    if (openingBrackets.includes(char)) {
+      // Если символ - открывающая скобка, добавляем его в стек.
+      stack.push(char);
+    } else if (closingBrackets.includes(char)) {
+      // Если символ - закрывающая скобка
+      const lastOpenBracket = stack.pop();
+      if (lastOpenBracket !== bracketMap.get(char)) {
+        // Если последняя открывающая скобка не соответствует текущей закрывающей, возвращаем false.
+        return false;
+      }
+    }
+  }
+
+  // Если стек пуст, то все скобки сбалансированы.
+  return stack.length === 0;
+};
+
+// не сделал на 100%, позде вернусь к задаче
